8a3df2fc080e1dca007a05ab0c81b3bf5ae053e3
caroline223 <carolineforrester22@gmail.com>
2020-10-28 00:10:22 -0400

    commit tree 35d3095f8f8fa1a98c2485df791f0da8cb263d91

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def roll_call_dwarves(name)
+  i = 0
+  while i < name.length 
+    puts "#{i +1}. #{name[i]}"
+    i += 1
+  end
+end
+roll_call_dwarves(dwarves)
+
+def summon_captain_planet(name)
+   new_array = [] 
+   i = 0
+   while i < name.length
+    new_array << name[i].capitalize + "!"
+    i += 1
+   end
+   new_array
+end
+summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(call)
+  call.any? do |word|
+    word.length > 4
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+
+def find_the_cheese(name)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  name.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert" 
+  end
 end
